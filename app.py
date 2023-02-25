fb1404f32cfce290af94c0bd6f284ae006a846f2
lalitabornare <122591953+lalitabornare@users.noreply.github.com>
2023-02-25 18:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file, Response
 from io import BytesIO
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///songs2.db'
@@ -42,6 +42,39 @@ def upload():
     return render_template('upload.html')
 
 
+@app.route('/delete/<int:song_id>', methods=['POST'])
+def delete(song_id):
+    upload = Song.query.filter_by(id=song_id).first()
+    db.session.delete(upload)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_term = request.form['query']
+        songs = Song.query.filter(
+            Song.title.ilike(f'%{search_term}%') |
+            Song.artist.ilike(f'%{search_term}%') |
+            Song.album.ilike(f'%{search_term}%')
+        ).all()
+        return render_template('search.html', songs=songs)
+    return redirect(url_for('index'))
+
+
+@app.route('/play/<int:song_id>')
+def play(song_id):
+    upload = Song.query.filter_by(id=song_id).first()
+    return Response(upload.data, mimetype='audio/mp3')
+
+
+@app.route('/download/<int:song_id>')
+def download(song_id):
+    upload = Song.query.filter_by(id=song_id).first()
+    return send_file(BytesIO(upload.data), download_name=upload.title, mimetype='audio/mp3', as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
